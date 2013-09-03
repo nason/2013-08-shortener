4e550162163ce4aa9346c0c2d9455277c0f927ac
Michael Nason <m.s.nason@gmail.com>
2013-09-03 15:30:11 -0700

    Add migration to setup links table

---
+++ db/migrate/002_update_links.rb
@@ -0,0 +1,17 @@
+# Put your database migration here!
+#
+# Each one needs to be named correctly:
+# timestamp_[action]_[class]
+#
+# and once a migration is run, a new one must
+# be created with a later timestamp.
+
+class UpdateLinks < ActiveRecord::Migration
+  def change
+    create_table :links do |t|
+      t.string :url
+      t.string :shortened
+      t.timestamps
+    end
+  end
+end
